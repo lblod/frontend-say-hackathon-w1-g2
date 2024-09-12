e4fd11656f34f83e582a0d0c87a6f1eacafad056
Claire Lovisa <lovisa.claire@gmail.com>
2024-09-12 20:26:07 +0200

    Add debounce and search on all text instead of just city

--- app/components/protected-monument-select.js
+++ app/components/protected-monument-select.js
@@ -1,15 +1,16 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import fetch from 'fetch';
-import { restartableTask } from 'ember-concurrency';
+import { restartableTask, timeout } from 'ember-concurrency';
 
 export default class AddressRegisterSelectorComponent extends Component {
   @service store;
 
   @restartableTask
   *search(searchData) {
+    yield timeout(400);
     const response = yield fetch(
-      `https://inventaris.onroerenderfgoed.be/erfgoedobjecten?gemeente=${searchData}`, {
+      `https://inventaris.onroerenderfgoed.be/erfgoedobjecten?tekst=${searchData}`, {
           headers: {
           'Accept': 'application/json',
         },
