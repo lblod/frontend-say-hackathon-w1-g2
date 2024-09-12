3db6cf8ad12fd98f31f4bcf0f00e7c32efd57a93
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-09-12 10:28:28 +0200

    add packages

--- app/router.js
+++ app/router.js
@@ -1,9 +1,18 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'frontend-say-hackathon-w1-g2/config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "frontend-say-hackathon-w1-g2/config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  // Auth
+  this.route("auth", { path: "/authorization" }, function () {
+    this.route("callback");
+    this.route("login");
+    this.route("logout");
+  });
+
+  this.route("requests");
+});
