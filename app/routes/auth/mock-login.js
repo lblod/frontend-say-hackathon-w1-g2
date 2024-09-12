c5663d55dc4db560f5b215fd5283eac6068e5f2f
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-09-12 11:17:00 +0200

    wip: mock login errors

--- app/routes/auth/mock-login.js
+++ app/routes/auth/mock-login.js
@@ -1,9 +1,9 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
+
 export default class MockLoginRoute extends Route {
-  @service() session;
-  @service() store;
-  @service() currentSession;
+  @service session;
+  @service store;
 
   queryParams = {
     page: {
@@ -11,36 +11,17 @@ export default class MockLoginRoute extends Route {
     },
   };
 
-  async beforeModel() {
-    if (this.session.isAuthenticated) {
-      await this.currentSession.load();
-      if (
-        ENV.controllerLogin === "true" &&
-        !this.currentSession.roles?.includes(ENV.roleClaim)
-      ) {
-        this.session.prohibitAuthentication("index");
-      }
-      if (this.session.isMockLoginSession) {
-        this.session.prohibitAuthentication("index");
-      }
-    }
+  beforeModel() {
+    this.session.prohibitAuthentication("index");
   }
 
   async model(params) {
-    const filter = { provider: "https://github.com/lblod/mock-login-service" };
-    if (params.gemeente) filter.user = { groups: params.gemeente };
-    try {
-      const accounts = await this.store.query("account", {
-        include: "user,user.groups",
-        filter: filter,
-        page: { size: 10, number: params.page },
-        sort: "user.family-name",
-      });
-      return accounts;
-    } catch (error) {
-      throw new Error("Something went wrong while fetching accounts", {
-        cause: error,
-      });
-    }
+    let accounts = await this.store.query("account", {
+      include: "user.groups",
+      filter: { provider: "https://github.com/lblod/mock-login-service" },
+      page: { size: 10, number: params.page },
+    });
+
+    return { accounts };
   }
 }
