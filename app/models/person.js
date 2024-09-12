a87a4d6133ca0421352ea79e9af7d19347777de0
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-09-12 11:21:00 +0200

    add models

---
+++ app/models/person.js
@@ -0,0 +1,30 @@
+import { attr, hasMany } from "@ember-data/model";
+import AgentModel from "./agent";
+
+export default class PersonModel extends AgentModel {
+  @attr givenName;
+  @attr familyName;
+  @attr firstNameUsed;
+
+  @hasMany("agent-in-position", {
+    inverse: "person",
+    async: true,
+    polymorphic: true,
+    as: "person",
+  })
+  agentsInPosition;
+
+  @hasMany("mandatory", {
+    inverse: "governingAlias",
+    async: true,
+    polymorphic: true,
+    as: "person",
+  })
+  mandatories;
+
+  @hasMany("functionary", {
+    inverse: "governingAlias",
+    async: true,
+  })
+  functionaries;
+}
