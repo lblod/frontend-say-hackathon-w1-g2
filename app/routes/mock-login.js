4b5a852fb1c87e5d7dbce36cd34f6f6e11427a7d
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-09-12 19:05:32 +0200

    Fix mock login users

--- app/routes/mock-login.js
+++ app/routes/mock-login.js
@@ -1,5 +1,5 @@
-import Route from "@ember/routing/route";
-import { inject as service } from "@ember/service";
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class MockLoginRoute extends Route {
   @service session;
@@ -11,6 +11,17 @@ export default class MockLoginRoute extends Route {
     },
   };
 
+  beforeModel() {
+    this.session.prohibitAuthentication('index');
+  }
 
+  async model(params) {
+    let accounts = await this.store.query('account', {
+      include: 'user,user.groups',
+      filter: { provider: 'https://github.com/lblod/mock-login-service' },
+      page: { size: 10, number: params.page },
+    });
 
+    return { accounts };
+  }
 }
