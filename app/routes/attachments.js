282d6a9ed245607e36e0c51c6abacb318896629f
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-09-13 14:08:40 +0200

    Link detail page

---
+++ app/routes/attachments.js
@@ -0,0 +1,19 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class AttachmentsRoute extends Route {
+  @service currentSession;
+  @service session;
+  @service store;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model(params) {
+    const model = await this.store.findRecord('application-case', params.id, {
+      include: 'designation-object',
+    });
+
+    return model;
+  }
+}
